61351ed297fa1f82ebca2e27e26181596aa6795e
zcschu01 <bbigboi65@gmail.com>
2020-04-07 17:14:11 -0400

    added more to survey

--- ParksapediaWeb/Classes/form.js
+++ ParksapediaWeb/Classes/form.js
@@ -1,14 +1,16 @@
 class form {
-    #location = null;
+    #region = null;
     #park_dog_friendly = false;
     #landscapes = [];
+    #wildlife = [];
+    #plants = [];
     constructor(){
         console.log("Made a new form");
     }
 
-    set_location(loc) {
-        this.#location = loc;
-        console.log("Location changed to " + this.#location);
+    set_region(r) {
+        this.#region = r;
+        console.log("Location changed to " + this.#region);
     }
 
     set_park_dog_friendly(pdf) {
@@ -19,5 +21,15 @@ class form {
     set_landscapes(ls) {
         this.#landscapes = ls;
         console.log("Landscapes changed to " + this.#landscapes);
+    }
+
+    set_wildlife(w) {
+        this.#wildlife = w;
+        console.log("Wildlife changed to " + this.#wildlife);
+    }
+
+    set_plants(p) {
+        this.#plants = p;
+        console.log("Plants changed to " + this.#plants);
     }
 }
--- ParksapediaWeb/scripts/validate.js
+++ ParksapediaWeb/scripts/validate.js
@@ -3,22 +3,32 @@ function validate() {
     // var locationValues = $('input[type=checkbox]:checked').map(function(_, el) {
     //     return $(el).val();
     // }).get();
-    var locationValue = $("input[name='region-r']:checked").val();
+    var regionValue = $("input[name='region-r']:checked").val();
     var dogValue = $("input[name='park-dog-friendly-r']:checked").val();
     var landscapeValues = $("input[name='landscape-c']:checked").map(function(_, el) {
         return $(el).val();
     }).get();
-    console.log(locationValue + " was clicked");
+    var wildlifeValues = $("input[name='wildlife-c']:checked").map(function(_, el) {
+        return $(el).val();
+    }).get();
+    var plantValues = $("input[name='plant-c']:checked").map(function(_, el) {
+        return $(el).val();
+    }).get();
+    console.log(regionValue + " was clicked");
     console.log(dogValue + " was clicked");
     console.log(landscapeValues + " was clicked");
+    console.log(wildlifeValues + " was clicked");
+    console.log(plantValues + " was clicked");
     
-    f.set_location(locationValue);
-    f.set_landscapes(landscapeValues);
-
+    f.set_region(regionValue);
     if(dogValue == "yes"){
         f.set_park_dog_friendly(true);
     }
     else{
         f.set_park_dog_friendly(false);
     }
+    f.set_landscapes(landscapeValues);
+    f.set_wildlife(wildlifeValues);
+    f.set_plants(plantValues);
+
 }
