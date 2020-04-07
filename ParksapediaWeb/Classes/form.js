2e1b218a2f2ff193fa45375374f4af2fea2a3282
zcschu01 <bbigboi65@gmail.com>
2020-04-07 14:46:09 -0400

    added in landscapes

--- ParksapediaWeb/Classes/form.js
+++ ParksapediaWeb/Classes/form.js
@@ -1,17 +1,23 @@
 class form {
-    #location = "";
+    #location = null;
     #park_dog_friendly = false;
-    constructor(){}
+    #landscapes = [];
+    constructor(){
+        console.log("Made a new form");
+    }
 
     set_location(loc) {
-        console.log("Location was " + this.#location);
         this.#location = loc;
         console.log("Location changed to " + this.#location);
     }
 
     set_park_dog_friendly(pdf) {
-        console.log("Location was " + this.#park_dog_friendly);
         this.#park_dog_friendly = pdf;
-        console.log("Location changed to " + this.#park_dog_friendly);
+        console.log("Friendly changed to " + this.#park_dog_friendly);
+    }
+
+    set_landscapes(ls) {
+        this.#landscapes = ls;
+        console.log("Landscapes changed to " + this.#landscapes);
     }
 }
--- ParksapediaWeb/scripts/validate.js
+++ ParksapediaWeb/scripts/validate.js
@@ -1,18 +1,24 @@
 function validate() {
     let f = new form();
-    var locationValue = $("input[name='location-r']:checked").val();
+    // var locationValues = $('input[type=checkbox]:checked').map(function(_, el) {
+    //     return $(el).val();
+    // }).get();
+    var locationValue = $("input[name='region-r']:checked").val();
     var dogValue = $("input[name='park-dog-friendly-r']:checked").val();
+    var landscapeValues = $("input[name='landscape-c']:checked").map(function(_, el) {
+        return $(el).val();
+    }).get();
     console.log(locationValue + " was clicked");
     console.log(dogValue + " was clicked");
+    console.log(landscapeValues + " was clicked");
     
-    //not working
-    f.set_location = locationValue;
+    f.set_location(locationValue);
+    f.set_landscapes(landscapeValues);
 
-    //not working
-    if(dogValue == "Yes"){
-        f.set_park_dog_friendly = true;
+    if(dogValue == "yes"){
+        f.set_park_dog_friendly(true);
     }
     else{
-        f.set_park_dog_friendly = false;
+        f.set_park_dog_friendly(false);
     }
 }
