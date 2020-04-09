170c632ce471068293ab6325c47d45c8f84c2812
Sai M Chinthala <csai1997@gmail.com>
2020-04-09 13:31:45 -0400

    adding queryies

--- ParksapediaAPI/Controllers/ParksController.cs
+++ ParksapediaAPI/Controllers/ParksController.cs
@@ -1,5 +1,6 @@
 ﻿using ParksapediaAPI.Classes;
 using ParksapediaAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -26,15 +27,109 @@ namespace ParksapediaAPI.Controllers
 
         [Route("api/SendForm")]
         [HttpPost]
-        public bool Post([FromBody]Form form)
+        public IEnumerable<string> Post([FromBody]Form form)
         {
-            if (form.landscapes != null &&
-                form.plants != null &&
-                form.region != null &&
-                form.wildlife != null)
-                return true;
-            else
-                return false;
+
+            var reg = form.region;
+
+            //narrow list of parks by region
+            List<string> parksToUse = new List<string>();
+            using (NationalParkContext ctx = new NationalParkContext())
+            {
+
+                var parksInRegion = ctx.Region.Where(r => r.Region1 == reg).ToList();
+
+                foreach (var p in parksInRegion)
+                {
+                    parksToUse.Add(p.ParkName);
+                }
+            }
+            //dog friendly, lodging and campgrounds - in the parks table
+            List<string> parkContraints = new List<string>();
+            using (NationalParkContext ctx = new NationalParkContext())
+            {
+                var camping = "no";
+                var lodging = "no";
+                var dogs = "no";
+                if (form.campgrounds)
+                {
+                    camping = "yes";
+                }
+                if (form.lodging)
+                {
+                    lodging = "yes";
+                }
+                if (form.park_dog_friendly)
+                {
+                    dogs = "yes";
+                }
+
+
+                var parks = ctx.Park.Where(r => (r.DogFriendly == dogs) || (r.DogFriendly == "restricted areas") && (r.Lodging == lodging) && (r.Camping == camping)).ToList();
+
+                foreach (var p in parks)
+                {
+                    parkContraints.Add(p.Park1);
+                }
+            }
+
+            var regionAndContraints = parksToUse.Union(parkContraints);
+
+            List<string> landscapeParks = new List<string>();
+            ///narrow list of parks by landscape
+
+            using (NationalParkContext ctx = new NationalParkContext())
+            {
+                foreach (var l in form.landscapes)
+                {
+                    var parksPerLandscape = ctx.Landscape.Where(r => r.Landscape1 == l).ToList();
+
+                    foreach (var lc in parksPerLandscape)
+                    {
+                        landscapeParks.Add(lc.ParkName);
+                    }
+                }
+            }
+
+            var regionAndLandscapesAndContraints = regionAndContraints.Union(landscapeParks);
+
+            List<string> parksByPlant = new List<string>();
+            //narrow list of parks by plants
+            using (NationalParkContext ctx = new NationalParkContext())
+            {
+                foreach (var p in form.plants)
+                {
+                    var parksPerPlantTag = ctx.Plant.Where(r => r.Tag == p).ToList();
+
+                    foreach (var pt in parksPerPlantTag)
+                    {
+                        parksByPlant.Add(pt.ParkName);
+                    }
+                }
+            }
+
+            var regionAndLandscapesAndContraintsAndPlants = regionAndLandscapesAndContraints.Union(parksByPlant);
+
+            //narrow list of parks by wildlife
+            List<string> parksByWildlife = new List<string>();
+            //narrow list of parks by plants
+            using (NationalParkContext ctx = new NationalParkContext())
+            {
+                foreach (var w in form.wildlife)
+                {
+                    var parksPerWildlifeTag = ctx.Wildlife.Where(r => r.Tag == w).ToList();
+
+                    foreach (var pt in parksPerWildlifeTag)
+                    {
+                        parksByWildlife.Add(pt.ParkName);
+                    }
+                }
+            }
+            var regionAndLandscapesAndContraintsAndPlantsAndWildlife = regionAndLandscapesAndContraintsAndPlants.Union(parksByWildlife);
+
+
+            return regionAndLandscapesAndContraintsAndPlantsAndWildlife;
+
         }
     }
 }
