49b6a0145287e6ef2be39432b9cfb9adfb0e44b8
Cole Schuhmann <bbigboi65@gmail.com>
2024-05-26 16:26:41 -0400

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   runtimeConfig: {
     // SSR keys
     NPS_API_KEY: '',
     public: {
       // CSR keys
     }
-  }
+  },
+
+  modules: ["@nuxtjs/tailwindcss"]
 })
